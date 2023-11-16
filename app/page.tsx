427eecc0163dfcfa82e39b8fa5b1c7686b24cb85
Jarle302 <jarlehtollaksen2@live.no>
2023-11-16 11:41:46 +0100

    add more style card

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Hero />
-      <Card/>
       <Products listOf={"Listings"} />
     </main>
   )
--- app/ui/components/Card.tsx
+++ app/ui/components/Card.tsx
@@ -6,34 +6,36 @@ import Image from 'next/image'
 import { lato } from '../fonts/fonts';
 
 
-const Card = ({ children, title, _count, media, endsAt }) => {
+const Card = ({ children, title, _count, media, endsAt, description }) => {
 
     const timeRemaining = getTimeRemaining(endsAt);
     return (
-        <div className=" w-[250px] h-[380px] rounded-2xl bg-gray-400 p-2 flex flex-col items-center">
-            <h3 className="my-2">{title}</h3>
-            <div className='flex flex-col border border-solid border-gray-600 w-full h-[337px] rounded-xl'>
-                {media && <img className='w-[250px] h-[180px] object-cover rounded-t-xl' src={media[0]} alt="Carousel Item" />}
-                <div className='flex flex-col items-center gap-3'>
-                    <div className='flex w-full justify-between'>
-                        <p className='text-orange-200 border border-solid border-gray-600 '>Bids {_count?.bids ? _count.bids : 0}</p>
-                        <p>
-                            {timeRemaining.days >= 0
-                                ? (
-                                    <>
-                                        <span className={`text-red-200 text-bold ${lato.className}`}>Ends in </span>
-                                        {timeRemaining.days} d {timeRemaining.hours} h {timeRemaining.minutes} m
-                                    </>
-                                )
-                                : "Ended"
-                            }
-                        </p>
+        <div className=" w-[250px] h-[380px] rounded-2xl bg-gray-400 flex flex-col items-center">
+            <div className='flex flex-col justify-between  w-full h-full rounded-xl  border-2 border-solid border-gray-600 border-t-none'>
+                <div>
+                    <h3 className="w-full bg-gray-700 mt-2.5 rounded-t-xl mb-3.25 self-center text-center text-white">{title}</h3>
+                    {media && <img className='w-[250px] h-[180px] object-cover' src={media[0]} alt="Carousel Item" />}
+                    <div className='flex flex-col items-center gap-3 '>
+                        <div className='flex w-full justify-between border border-solid border-gray-600 bg-gray-600 text-white p-1'>
+                            <p className='text-orange-200 '>Bids {_count?.bids ? _count.bids : 0}</p>
+                            <p>
+                                {timeRemaining.days >= 0
+                                    ? (
+                                        <>
+                                            <span className={`text-red-200 text-bold ${lato.className}`}>Ends in </span>
+                                            {timeRemaining.days} d {timeRemaining.hours} h {timeRemaining.minutes} m
+                                        </>
+                                    )
+                                    : "Ended"
+                                }
+                            </p>
+                        </div>
+                        <p className="pl-1 max-w-[223px] max-h-[43px] self-start overflow-hidden">{description}</p>
                     </div>
-                    <p className="pl-1 max-w-[223px] max-h-[43px] self-start overflow-hidden">testtesttesttestt esttesttesttest testtesttesttestte sttesttesttes ttesttesttesttesttesttesttesttesttesttesttesttest</p>
-                    <div className="flex w-full mh-auto mt-[22px]">
-                        <button className='py-[4px] w-1/2 rounded-tl-xl rounded-bl-xl bg-red-200' onClick={() => { console.log("hello") }}>Bid</button>
-                        <button className="py-[4px] w-1/2 rounded-br-xl rounded-tr-xl border-solid border-2  border-red-200" onClick={() => { console.log("hello") }} isSecondary={true}>Read more</button>
-                    </div>
+                </div>
+                <div className="flex w-full">
+                    <input className='py-[4px] w-1/2 rounded-tl-xl rounded-bl-xl border-solid border-2  border-red-200' type="number" name='bidAmount' id='bidAmount' />
+                    <button className="py-[4px] w-1/2 rounded-br-xl rounded-tr-xl bg-red-200" onClick={() => { console.log("hello") }} isSecondary={true}>Bid now</button>
                 </div>
             </div>
         </div>
