9fd8426f0a07158fc70f28cf071f306faf6775a1
Jarle302 <jarlehtollaksen2@live.no>
2023-11-17 20:42:24 +0100

    bugfix

--- app/ui/components/Card.tsx
+++ app/ui/components/Card.tsx
@@ -14,6 +14,7 @@ type CardProps = {
 };
 
 const Card = ({ children, title, _count, media, endsAt, description }: CardProps) => {
+    type ImageComponentProps = { src: string, width?: string, height?: string }[]
     let imageProps = [{ src: media[0] }]
     if (media.length > 1) {
         imageProps = media.map((image) => ({ src: image, width: "183px", height: "180px" }))
--- app/ui/components/ImageComponent.tsx
+++ app/ui/components/ImageComponent.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 type ImageComponentProps = {
-    src: string;
+    src: ?string;
     alt?: string;
     width?: number;
     height?: number;
