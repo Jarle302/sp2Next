65361dea49d85053d301ad9c81042a008076c96c
Jarle302 <jarlehtollaksen2@live.no>
2023-11-18 08:53:15 +0100

    add fix to nav

--- app/ui/components/Nav.tsx
+++ app/ui/components/Nav.tsx
@@ -2,22 +2,30 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation'
+import { RxHamburgerMenu } from "react-icons/rx";
+import { useState } from "react"
+
 
 
 const Nav = () => {
+    const [hidden, setHidden] = useState(true)
+    let hiddenClass = hidden ? "hidden" : ""
     const pathName = usePathname()
     return (
-        <header >
-            <nav className='flex justify-between items-center px-8 py-2'>
-                <Image className="rounded-xl" src="/eksamenlogo.png" width={100} height={93} alt="Logo" />
+        <header className=" fixed md:static w-full h-[74px] bg-gray-600 z-20">
+            <nav className='flex justify-between items-center px-8 py-2 fixed md:static'>
+                <Image className="rounded-xl" src="/eksamenlogo.png" width={60} height={93} alt="Logo" />
                 <div><input className="rounded-2xl" type="text" name="searchBar" id="searchBar" /> <button></button></div>
-                <ul className='flex gap-8 '>
+                <ul className={`${hiddenClass} md:flex gap-8  `}>
                     <li className={pathName === "/" ? "bg-red-200" : ""}> <Link href="./">Home</Link> </li>
                     <li className={pathName === "/about" ? "bg-red-200" : ""}><Link href="./about">About</Link></li>
                     <li className={pathName === "/contact" ? "bg-red-200" : ""}><Link href="./contact">Contact</Link></li>
                 </ul>
+                <button onClick={() => {
+                    setHidden(prev => !prev)
+                }} className="md:hidden"><RxHamburgerMenu /></button>
             </nav>
-        </header>
+        </header >
     )
 }
 
