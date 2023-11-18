07b99b2655baa6bd2c8e8686baf6c1739c9ba056
Jarle302 <jarlehtollaksen2@live.no>
2023-11-18 07:49:43 +0100

    bugfix

--- app/ui/components/Hero.tsx
+++ app/ui/components/Hero.tsx
@@ -12,7 +12,7 @@ export default function Hero() {
         <section className="flex gap-10 justify-between flex-wrap">
             <div className='min-w-[300px] flex-1  flex gap-3 flex-col'>
                 <h1 className={`${playfair.className} font-bold text-gray-600 fs-3 custom-responsive-font border-b-2 border-red-200 rounded-2xl max-w-max`}>Collective chest</h1>
-                <div className='flex justify-between mt-8 items-center'>
+                <div className='flex justify-between mt-8 items-center flex-wrap'>
                     <div className='flex flex-col gap-8'>
                         <h2 className={`${lato.className} self-start font-bold text-gray-600 text-3xl`}>Shared Passion, Exceptional Finds</h2>
                         <p className='max-w-[400px] leading-relaxed'>Explore <strong>CommunityChest</strong> for a <em>fun and easy</em> auction experience! Find <strong>unique art</strong>, <em>vintage treasures</em>, and more in our lively online community. Start your treasure hunt with us <strong>today</strong>!</p>
