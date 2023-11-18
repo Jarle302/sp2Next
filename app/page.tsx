55aed3f6993cec468752559115524324bf59a9d3
Jarle302 <jarlehtollaksen2@live.no>
2023-11-18 09:32:04 +0100

    make logo link to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen text-gray-600 flex-col items-center justify-between p-24">
       <Hero />
       <Products listOf={"Listings"} />
     </main>
