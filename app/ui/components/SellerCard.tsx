246d2cb35dc80f806c42cd71e26057b05fb1117e
Jarle302 <jarlehtollaksen2@live.no>
2023-11-19 17:20:45 +0100

    bugfix

--- app/ui/components/SellerCard.tsx
+++ app/ui/components/SellerCard.tsx
@@ -1,5 +1,5 @@
-
-const SellerCard = ({ name, email, avatar }) => {
+type Seller = { name: string, email: string, avatar: string }
+const SellerCard = ({ name, email, avatar }:Seller) => {
 
     const placeholder = "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
     return (
