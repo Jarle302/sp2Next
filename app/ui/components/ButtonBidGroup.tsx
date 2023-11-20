798fb144eb96017cb2ad854bfb364cce001f1001
Jarle302 <jarlehtollaksen2@live.no>
2023-11-20 09:09:36 +0100

    bugfix

--- app/ui/components/ButtonBidGroup.tsx
+++ app/ui/components/ButtonBidGroup.tsx
@@ -1,14 +1,13 @@
 "use client"
 type buttongrp = {
     children: React.ReactNode;
-    handler: () => void;
 }
 
-const ButtonBidGrp = ({ handler, children }: buttongrp) => {
+const ButtonBidGrp = ({children }: buttongrp) => {
     return (
         <div className="flex w-full">
             <input className='py-[10px] w-1/2 rounded-tl-xl rounded-bl-xl border-solid border-2  border-red-200' type="number" name='bidAmount' id='bidAmount' />
-            <button className="py-[10px] w-1/2 rounded-br-xl rounded-tr-xl bg-red-200" onClick={handler}>{children}</button>
+            <button onClick={()=>console.log("test")} className="py-[10px] w-1/2 rounded-br-xl rounded-tr-xl bg-red-200"></button>
         </div>)
 }
 
