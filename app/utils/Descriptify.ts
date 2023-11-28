b8c8ca560cafba65c6d8f727c1d1bca4c411d2a1
Jarle302 <jarlehtollaksen2@live.no>
2023-11-28 14:30:41 +0100

    test

--- app/utils/Descriptify.ts
+++ app/utils/Descriptify.ts
@@ -7,19 +7,16 @@ const Descriptify = async ({
   title: string | undefined;
   description: string | undefined;
 }) => {
-  const data = await fetchFunction(
-    process.env.NEXT_PUBLIC_DESCRIPTIFY || "",
-    "POST",
-    {
-      body: ` Rewrite the following title and description into a more engaging and concise format for an auction listing, ensuring it is appealing and likely to attract buyers. Keep the rewritten description short and suitable for display in a paragraph element on a website.
+  const url = process.env.NEXT_PUBLIC_DESCRIPTIFY || "";
+  const data = await fetchFunction(url, "POST", {
+    body: ` Rewrite the following title and description into a more engaging and concise format for an auction listing, ensuring it is appealing and likely to attract buyers. Keep the rewritten description short and suitable for display in a paragraph element on a website.
 
     Title: ${title}
     Description: ${description}
     
     New Auction Listing Description:
     `,
-    }
-  );
+  });
 
   return data;
 };
