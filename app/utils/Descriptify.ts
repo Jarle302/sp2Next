37f7dd9b7f067f245e63fb73c15ac3143baa3b4e
Jarle302 <jarlehtollaksen2@live.no>
2023-11-28 15:00:21 +0100

    fix bug

--- app/utils/Descriptify.ts
+++ app/utils/Descriptify.ts
@@ -7,7 +7,7 @@ const Descriptify = async ({
   title: string | undefined;
   description: string | undefined;
 }) => {
-  const url = "http://localhost:3000/ai";
+  const url = process.env.NEXT_PUBLIC_DESCRIPTIFY;
   const data = await fetchFunction(url, "POST", {
     body: ` Rewrite the following title and description into a more engaging and concise format for an auction listing, ensuring it is appealing and likely to attract buyers. Keep the rewritten description short and suitable for display in a paragraph element on a website.
 
