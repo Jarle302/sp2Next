a431d4fc1aff401842e0af663c5f4f9b6bb7fb7c
Jarle302 <jarlehtollaksen2@live.no>
2023-12-06 09:15:28 +0100

    bugfix

--- app/ui/components/CardPreview.tsx
+++ app/ui/components/CardPreview.tsx
@@ -1,24 +1,48 @@
 import Card from "./Card";
 
+type Bid = {
+  id: string;
+  amount: number;
+  bidderName: string;
+  created: string;
+};
 
+type Seller = {
+  name: string;
+  email: string;
+  avatar: string;
+};
 
-type props ={ [key:string]:string|string[]|undefined
-    title: string;
-    description?: string;
-    tags?: string[];
-    media?: string[];
-    endsAt: string;
-   }
+type CardProps = {
+  title: string;
+  description?: string;
+  tags?: string[];
+  media?: string[];
+  endsAt: string;
+  id: string;
+  bids: Bid[];
+  seller: Seller;
+  _count: { bids: number };
+};
 
-const CardPreview = ({ title, media = [], endsAt, description="Your description" }:props) => {
-    const placeholderCount = {bids:0}
+const CardPreview = ({
+  title,
+  media = [],
+  endsAt,
+  description = "Your description",
+}: CardProps) => {
+  const placeholderCount = { bids: 0 };
 
-return(
-    <Card  id="0002" title={title} media={media} endsAt={endsAt} description={description}  _count={placeholderCount} />
-)
+  return (
+    <Card
+      id="0002"
+      title={title}
+      media={media}
+      endsAt={endsAt}
+      description={description}
+      _count={placeholderCount}
+    />
+  );
+};
 
-
-}
-
-
-export default CardPreview
+export default CardPreview;
