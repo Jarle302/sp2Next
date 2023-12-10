2601c6c6f181873b1233be81e1a9dc85ea93d6d8
Jarle302 <jarlehtollaksen2@live.no>
2023-12-10 11:50:36 +0100

    update

--- app/ui/components/DeleteButton.tsx
+++ app/ui/components/DeleteButton.tsx
@@ -1,4 +1,7 @@
 import deleteListing from "@/app/utils/api/deleteListing";
+import ListingReducerContext from "./ListingReducerContext";
+import { ReducerContext } from "./ListingReducerContext";
+import { useContext } from "react";
 
 export default function DeleteButton({
   id,
@@ -7,8 +10,15 @@ export default function DeleteButton({
   id: string;
   className: string;
 }) {
+  const { state, dispatch } = useContext(ReducerContext);
+
   return (
-    <button className={className} onClick={() => deleteListing(id)}>
+    <button
+      className={className}
+      onClick={async () => {
+        const status = await deleteListing(id);
+        console.log(status);
+      }}>
       Delete
     </button>
   );
