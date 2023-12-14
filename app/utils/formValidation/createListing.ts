6bedd334c881a3b404a02822bee0be73355db4cf
Jarle302 <jarlehtollaksen2@live.no>
2023-12-14 14:11:53 +0100

    add more debouncer to form

--- app/utils/formValidation/createListing.ts
+++ app/utils/formValidation/createListing.ts
@@ -1,40 +1,42 @@
-export default function validateCreateListing({
-  title,
-  description,
-  tags,
-  media,
-  endsAt,
-}: {
-  title: string;
-  description: string;
-  tags: string[];
-  media: string[];
-  endsAt: string;
-}) {
-  const errors = {
-    title: "",
-    description: "",
-    tags: [""],
-    media: [""],
-    endsAt: "",
-  };
-  if (title.length < 1) {
-    errors.title = "Title is required";
+export default function validateCreateListing(
+  inputName: string,
+  value: string
+) {
+  const errors: { [key: string]: string } = {};
+
+  switch (inputName) {
+    case "title": {
+      if (value?.length < 1) {
+        errors[inputName] = "Title is required";
+      }
+      return errors;
+    }
+    case "description": {
+      if (value?.length < 1) {
+        errors[inputName] =
+          "Description is optional but reccomended, if you need help write a short description you can let the ai help you";
+      }
+      return errors;
+    }
+    case "tags": {
+      if (value?.length < 1) {
+        errors[inputName] =
+          "Tags are optional but it's reccomended to include atleast one for best results";
+      }
+      return errors;
+    }
+    case "media": {
+      if (value?.length < 1) {
+        errors[inputName] =
+          "Media is optional, but is adviced to include a picture(validURL) for best results";
+      }
+      return errors;
+    }
+    case "endsAt": {
+      if (value?.length < 1) {
+        errors[inputName] = "End date is required";
+      }
+      return errors;
+    }
   }
-  if (description.length < 1) {
-    errors.description =
-      "Write a short description, best result is if you write a short description and let the ai help if you need help.";
-  }
-  if (tags.length < 1) {
-    errors.tags[0] =
-      "Tags are optional but it's reccomended to include atleast one for best results";
-  }
-  if (media.length < 1) {
-    errors.media[0] =
-      "Media is optional, but is adviced to include a picture(validURL) for best results";
-  }
-  if (endsAt?.length < 1) {
-    errors.endsAt = "End date is required";
-  }
-  return errors;
 }
