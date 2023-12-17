31da448f79da928fcb736e061312b63e18a6567d
Jarle302 <jarlehtollaksen2@live.no>
2023-12-17 22:28:21 +0100

    fix width searchbar

--- app/ui/components/Searchbar.tsx
+++ app/ui/components/Searchbar.tsx
@@ -7,7 +7,13 @@ import Link from "next/link";
 import { IoMdCloseCircle } from "react-icons/io";
 import { ListingProps } from "@/app/utils/types/types";
 
-const Searchbar = ({ Component,useCase }: { Component?: React.FC<ListingProps>,useCase:string }) => {
+const Searchbar = ({
+  Component,
+  useCase,
+}: {
+  Component?: React.FC<ListingProps>;
+  useCase: string;
+}) => {
   const { state, dispatch } = useContext(ReducerContext);
   const [searchQuery, handleChange, reset, setSearchQuery] = useForm(
     {
@@ -26,21 +32,25 @@ const Searchbar = ({ Component,useCase }: { Component?: React.FC<ListingProps>,u
   }
 
   return (
-    <section className=" relative flex-wrap flex justify-between">
-      <div className="basis-full m-auto">
-        <form >
-          <div className="flex bg-white  text-gray-600 mx-2 relative">
-            <label htmlFor={`search${useCase}`} className="font-bold py-2 absolute bottom-[20px] ">Search</label>
+    <div className=" relative flex-wrap flex justify-">
+      <div className=" shrink-0 grow-0">
+        <form>
+          <div className="  flex bg-white    my-3 text-gray-600  relative">
+            <label
+              htmlFor={`search${useCase}`}
+              className="font-bold py-2 absolute bottom-[20px] ">
+              Search
+            </label>
             <input
-              className="flex-basis-[100%] py-3 outline-none"
+              className="flex-basis-[100%] py-3 outline-none "
               onChange={handleChange}
               type="text"
-              name={`search${useCase}`}
+              name={`search`}
               id={`search${useCase}`}
               value={searchQuery.search}
             />
             <button
-            aria-label="Reset search"
+              aria-label="Reset search"
               className="text-[2rem]"
               onClick={(e) => {
                 e.preventDefault();
@@ -62,14 +72,14 @@ const Searchbar = ({ Component,useCase }: { Component?: React.FC<ListingProps>,u
           ))}
         </div>
       ) : (
-        <div className="overflow-y-auto flex flex-col md:flex-row md:flex-wrap gap-[20px]">
+        <div className="overflow-y-auto flex flex-col mt-5 md:flex-row md:flex-wrap gap-[20px]">
           {typeof Component !== "undefined" &&
             filteredListings.map((listing, index) => (
               <Component key={index} {...listing} />
             ))}
         </div>
       )}
-    </section>
+    </div>
   );
 };
 
